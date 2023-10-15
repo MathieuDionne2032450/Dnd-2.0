3ddaffd909b4f6efa41680326d0cf584f69ea17d
louisgaumond <2033838@etu.cchic.ca>
2023-10-15 16:59:41 -0400

    controllers louis
    me reste à faire pnj et race

--- Api_DnD_project/Api_DnD/Controllers/PersoController.cs
+++ Api_DnD_project/Api_DnD/Controllers/PersoController.cs
@@ -21,14 +21,57 @@ namespace Api_DnD.Controllers
 
         // GET: PersoController/Create
         [HttpGet("/AllPerso")]
-        public async Task<ActionResult<IEnumerable<Perso>>> GetAllPerso()
+        public async Task<ActionResult<IEnumerable<Perso>>> GetAllPerso(string? sortOrder, string? recherche, string? rechercheCampagne, int page)
         {
+            if (!string.IsNullOrEmpty(recherche))
+            {
+                return await _context.Persos.Where(p => p.Nom.Contains(recherche)).ToListAsync();
+            }
 
-            return await _context.Persos
-                .Include(p => p.LesArmes)
-                .ThenInclude(p => p.Enchantement)
-                .OrderBy(p => p.Nom)
-                .ToListAsync();
+            if (!string.IsNullOrEmpty(rechercheCampagne))
+            {
+                List<Perso> listePersos = new List<Perso>();
+                foreach (Perso perso in _context.Persos)
+                {
+                    foreach (Campagne campagne in perso.Campagne)
+                    {
+                        if (campagne.Name.Contains(rechercheCampagne))
+                            listePersos.Add(perso);
+                    }
+                }
+                return listePersos;
+            }
+
+            if (page <= 0)
+                page = 1;
+
+            switch (sortOrder) 
+            {
+                case "irlJoueur":
+                    return await _context.Persos.OrderBy(p => p.IrlJoueur).Skip((3 * page) - 3).Take(3).ToListAsync();
+                case "irlJoueur_desc":
+                    return await _context.Persos.OrderBy(p => p.IrlJoueur).Skip((3 * page) - 3).Take(3).ToListAsync();
+                case "nom":
+                    return await _context.Persos.OrderBy(p => p.Nom).Skip((3 * page) - 3).Take(3).ToListAsync();
+                case "nom_desc":
+                    return await _context.Persos.OrderByDescending(p => p.Nom).Skip((3 * page) - 3).Take(3).ToListAsync();
+                case "niv":
+                    return await _context.Persos.OrderBy(p => p.Niv).Skip((3 * page) - 3).Take(3).ToListAsync();
+                case "niv_desc":
+                    return await _context.Persos.OrderByDescending(p => p.Niv).Skip((3 * page) - 3).Take(3).ToListAsync();
+                case "id":
+                    return await _context.Persos.OrderBy(p => p.id).Skip((3 * page) - 3).Take(3).ToListAsync();
+                case "id_desc":
+                    return await _context.Persos.OrderByDescending(p => p.id).Skip((3 * page) - 3).Take(3).ToListAsync();
+                default:
+                    return await _context.Persos.Skip((3 * page) - 3).Take(3).ToListAsync();
+            }
+        }
+
+        [HttpGet("/GetPersoById/{id}")]
+        public async Task<ActionResult<Perso>> GetPersoById(int id)
+        {
+            return await _context.Persos.FindAsync(id);
         }
 
         // GET: PersoController/Create
@@ -43,6 +86,102 @@ namespace Api_DnD.Controllers
             }
 
             return PersoDTO.PersoToDTO(perso);
+        }
+
+        [HttpPut("/EditPerso")]
+        public async Task<ActionResult<Perso>> EditPerso(
+            int id, 
+            string IrlJoueur,
+            string Nom,
+            string Description,
+            int Inspiration,
+            int ArmureId,
+            ICollection<Arme> LesArmes,
+            int ClasseId,
+            int RaceId,
+            ICollection<Skill> Skills,
+            string Personalitetrait,
+            string Ideal,
+            string Bonds,
+            string Flaws,
+            int Niv,
+            ICollection<Campagne> Campagne)
+        {
+            await _context.Persos.Where(p => p.id == id).ExecuteUpdateAsync(setters => setters
+            .SetProperty(p => p.IrlJoueur, IrlJoueur)
+            .SetProperty(p => p.Nom, Nom)
+            .SetProperty(p => p.Description, Description)
+            .SetProperty(p => p.Inspiration, Inspiration)
+            .SetProperty(p => p.ArmureId, ArmureId)
+            .SetProperty(p => p.LesArmes, LesArmes)
+            .SetProperty(p => p.ClasseId, ClasseId)
+            .SetProperty(p => p.RaceId, RaceId)
+            .SetProperty(p => p.Skills, Skills)
+            .SetProperty(p => p.Personalitetrait, Personalitetrait)
+            .SetProperty(p => p.Ideal, Ideal)
+            .SetProperty(p => p.Bonds, Bonds)
+            .SetProperty(p => p.Flaws, Flaws)
+            .SetProperty(p => p.Niv, Niv)
+            .SetProperty(p => p.Campagne, Campagne)
+            );
+
+            return NoContent();
+        }
+
+        [HttpPost("/CreatePerso")]
+        public async Task<ActionResult<Perso>> CreatePerso(
+            string IrlJoueur,
+            string Nom,
+            string Description,
+            int Inspiration,
+            int ArmureId,
+            ICollection<Arme> LesArmes,
+            int ClasseId,
+            int RaceId,
+            ICollection<Skill> Skills,
+            string Personalitetrait,
+            string Ideal,
+            string Bonds,
+            string Flaws,
+            int Niv,
+            ICollection<Campagne> Campagne)
+        {
+            Perso perso = new Perso
+            {
+                IrlJoueur = IrlJoueur,
+                Nom = Nom,
+                Description = Description,
+                Inspiration = Inspiration,
+                ArmureId = ArmureId,
+                LesArmes = LesArmes,
+                ClasseId = ClasseId,
+                RaceId = RaceId,
+                Skills = Skills,
+                Personalitetrait = Personalitetrait,
+                Ideal = Ideal,
+                Bonds = Bonds,
+                Flaws = Flaws,
+                Niv = Niv,
+                Campagne = Campagne
+            };
+
+            _context.Persos.Add(perso);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPersoById", new { id = perso.id }, perso);
+        }
+
+        [HttpDelete("/DeletePerso/{id}")]
+        public async Task<bool> DeletePerso(int id)
+        {
+            if (await _context.Persos.Where(p => p.id.Equals(id)).ExecuteDeleteAsync() == 1) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- Api_DnD_project/Api_DnD/Controllers/RaceController.cs
+++ Api_DnD_project/Api_DnD/Controllers/RaceController.cs
@@ -20,9 +20,93 @@ namespace Api_DnD.Controllers
 
         // GET: api/<RaceController>
         [HttpGet("GetAllRace")]
-        public async Task<ActionResult<IEnumerable<Race>>> GetRace()
+        public async Task<ActionResult<IEnumerable<Race>>> GetRace(string? sortOrder, string? recherche, string? rechercheCampagne, int page)
         {
-            return await _context.Races.OrderBy(r => r.Nom).ToListAsync();
+            if(!string.IsNullOrEmpty(recherche))
+            {
+                return await _context.Races.Where(r => r.Nom.Contains(recherche)).ToListAsync();
+            }
+
+            // Si le nom d'une campagne est entré, passer à travers toutes les races pour voir si elles font partie de celle-ci
+            if(!string.IsNullOrEmpty(rechercheCampagne))
+            {
+                List<Race> listeRaces = new List<Race>();
+                foreach(Race race in _context.Races)
+                {
+                    foreach(Campagne campagne in race.Campagnes)
+                    {
+                        // Si le nom de telle campagne de la race actuelle contient les critères de recherche
+                        // Ajouter la race à la liste de races à retourner
+                        if (campagne.Name.Contains(rechercheCampagne))
+                            listeRaces.Add(race);
+                    }
+                }
+                return listeRaces;
+            }
+
+            if (page <= 0)
+                page = 1;
+
+            switch (sortOrder)
+            {
+                case "nom":
+                    return await _context.Races.OrderBy(r => r.Nom).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                case "nom_desc":
+                    return await _context.Races.OrderByDescending(r => r.Nom).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                case "bonusPV":
+                    return await _context.Races.OrderBy(r => r.BonusPV).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                case "bonusPV_desc":
+                    return await _context.Races.OrderByDescending(r => r.BonusPV).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                case "bonusDex":
+                    return await _context.Races.OrderBy(r => r.BonusDex).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                case "bonusDex_desc":
+                    return await _context.Races.OrderByDescending(r => r.BonusDex).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                case "bonusForce":
+                    return await _context.Races.OrderBy(r => r.BonusForce).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                case "bonusForce_desc":
+                    return await _context.Races.OrderByDescending(r => r.BonusForce).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                case "bonusIntel":
+                    return await _context.Races.OrderBy(r => r.BonusIntel).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                case "bonusIntel_desc":
+                    return await _context.Races.OrderByDescending(r => r.BonusIntel).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                case "bonusWisdom":
+                    return await _context.Races.OrderBy(r => r.BonusWisdom).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                case "bonusWisdom_desc":
+                    return await _context.Races.OrderByDescending(r => r.BonusWisdom).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                case "bonusConsti":
+                    return await _context.Races.OrderBy(r => r.BonusConsti).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                case "bonusConsti_desc":
+                    return await _context.Races.OrderByDescending(r => r.BonusConsti).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                case "bonusCharisma":
+                    return await _context.Races.OrderBy(r => r.BonusCharisma).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                case "bonusCharisma_desc":
+                    return await _context.Races.OrderByDescending(r => r.BonusCharisma).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                case "id":
+                    return await _context.Races.OrderBy(r => r.Id).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                case "id_desc":
+                    return await _context.Races.OrderByDescending(r => r.Id).Skip((3 * page) - 3).Take(3).ToListAsync();
+                    break;
+                default:
+                    return await _context.Races.Skip((3 * page) - 3).Take(3).ToListAsync();
+            }
+
         }
 
         // GET api/<RaceController>/5
@@ -105,7 +189,7 @@ namespace Api_DnD.Controllers
         }
 
         // PUT api/<RaceController>/5
-        [HttpPut("CreateRace")]
+        [HttpPost("CreateRace")]
         public void Post(int id, string nom, int bonusPv, int bonusDex, int bonusForce, int bonusIntel, int bonusWisdom, int bonusConsti, int bonusCharisma, int campagne)
         {
             Race race = new Race {Id = id, Nom = nom, BonusPV = bonusPv, BonusDex = bonusDex, BonusIntel = bonusIntel, BonusForce = bonusForce, BonusWisdom = bonusWisdom, BonusConsti = bonusConsti, BonusCharisma = bonusCharisma};
@@ -116,8 +200,16 @@ namespace Api_DnD.Controllers
 
         // DELETE api/<RaceController>/5
         [HttpDelete("DeleteRace")]
-        public void Delete(int id)
+        public async Task<bool> DeleteRace(int id)
         {
+            if (await _context.Races.Where(r => r.Id.Equals(id)).ExecuteDeleteAsync() == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
