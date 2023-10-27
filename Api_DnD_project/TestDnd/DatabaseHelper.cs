3b51e4bd54f82911c9b9d2dc57966021dcf13f50
Alexandre1832686 <1832686@etu.cchic.ca>
2023-10-27 17:11:22 -0400

    alex 27 2

--- Api_DnD_project/TestDnd/DatabaseHelper.cs
+++ Api_DnD_project/TestDnd/DatabaseHelper.cs
@@ -229,7 +229,7 @@ namespace TestDnd
             context.Races.Add(race);
             context.Persos.Add(perso);
 
-            foreach(Skill s in skills)
+            foreach (Skill s in skills)
             {
                 context.Skill.Add(s);
             }
@@ -300,6 +300,10 @@ namespace TestDnd
         {
             context.Database.EnsureCreated();
 
+
+
+
+
             feats[] featslist = new feats[]
             {
                 new feats { id = 1, Nom = "Artificier", Descr = "Peut tirer du cannon" },
@@ -312,6 +316,125 @@ namespace TestDnd
             foreach (feats feats in featslist)
             {
                 context.Feats.Add(feats);
+            }
+
+            context.SaveChanges();
+        }
+
+        #endregion
+
+        #region Spell
+
+        public void CreateTablesSpell()
+        {
+            context.Database.EnsureCreated();
+
+            Campagne campagne = new Campagne
+            {
+                Id = 1,
+                Name = "La campagne",
+                Desc = "La campagne principale",
+                Armes = new List<Arme>(),
+                Armures = new List<Armure>(),
+                Enchantements = new List<Enchantement>(),
+                Monstres = new List<Monstre>(),
+                PNJs = new List<PNJ>(),
+                Quetes = new List<Quete>(),
+                Classes = new List<Classes>(),
+                Races = new List<Race>()
+            };
+
+            Enchantement enchantement = new Enchantement
+            {
+                Nom = "Enchantement",
+                Description = "Enchantement cool",
+                Type = "Enchanté",
+                Modif = 1,
+                Id = 1
+            };
+
+            Armure armure = new Armure
+            {
+                Name = "Armure",
+                Type = "Forte",
+                Ac = 2,
+                DexBonus = true,
+                MaxDexMod = 2,
+                StealthDisadvantage = 2,
+                EnchantementId = 1
+            };
+
+            Arme arme = new Arme
+            {
+                BonusJet = 2,
+                BonusForce = 2,
+                Nom = "Épée",
+                EnchantementId = 1,
+                Campagne = new List<Campagne> { campagne }
+            };
+
+            Classes classes = new Classes
+            {
+                name = "Classe",
+                description = "Description de la classe",
+                hitDie = "8d6",
+                primaryAbility = "Habileté primaire",
+                id = 1,
+                Campagne = new List<Campagne> { campagne }
+            };
+
+            Race race = new Race
+            {
+                Nom = "Orque",
+                BonusPV = 1,
+                BonusDex = 1,
+                BonusForce = 1,
+                BonusIntel = 1,
+                BonusWisdom = 1,
+                BonusConsti = 1,
+                BonusCharisma = 1,
+                Id = 1,
+                Campagnes = new List<Campagne> { campagne }
+            };
+
+            Perso perso = new Perso
+            {
+                IrlJoueur = "Alexandre",
+                Nom = "Krados",
+                Description = "démon démoniaque",
+                Inspiration = 0,
+                ArmureId = 1,
+                LesArmes = new List<Arme> { arme },
+                ClasseId = 1,
+                RaceId = 1,
+                Personalitetrait = "evil",
+                Ideal = "Justice",
+                Bonds = "Avec Louise",
+                Flaws = "Le bruit",
+                Niv = 1,
+                CampagneId = 1
+            };
+
+            List<Perso> persos = new List<Perso> { perso };
+
+            Spell[] Spelllist = new Spell[]
+            {
+                new Spell() { id = 1, Name = "Lazer de feu", Description = "tire un lazer de feu sur 1 énemi",DammageType="Feu",Dammage=20,ClassId=1,Zone="Cible" },
+                new Spell() { id = 2, Name = "Foudre des dieux", Description = "Fait tomber la foudre sur un cible avec un rayon de 20ft",DammageType="Foudre",Dammage=75,ClassId=1,Zone="20ft" },
+
+            };
+
+            context.Campagnes.Add(campagne);
+            context.Enchantements.Add(enchantement);
+            context.Armures.Add(armure);
+            context.Armes.Add(arme);
+            context.Classes.Add(classes);
+            context.Races.Add(race);
+            context.Persos.Add(perso);
+
+            foreach (Spell s in Spelllist)
+            {
+                context.Spells.Add(s);
             }
 
             context.SaveChanges();
